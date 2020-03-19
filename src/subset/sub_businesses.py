ff4f2ff4fe0837a9dc7a3c6ecc1d157ac70c1301
Andrei Michael Dreyer <19809301@sun.ac.za>
2020-03-19 13:08:02 +0200

    Updated business subset to only choose businesses that have photos with captions

--- src/subset/sub_businesses.py
+++ src/subset/sub_businesses.py
@@ -1,5 +1,8 @@
 from tqdm import tqdm
 import config
+import json
+
+from norm import normalize_photos
 
 SUB_FILE = "./out/business_subset_{}.json".format(config.SUBSET_SETTINGS['PERC'])
 
@@ -10,11 +13,23 @@ def generate_subset(f_dir, perc):
     num_lines = sum(1 for _ in fr)
     sub_num = int(perc * num_lines)
 
+    n_photos_f = open(normalize_photos.NORM_FILE, 'r')
+    
+    photo_info = []
+
+    for line in n_photos_f:
+        json_info = json.loads(line)
+        if json_info['caption'] != '':
+            photo_info.append((json_info['business_id']))
+
     fr.seek(0)
     with tqdm(total=sub_num) as pbar:
         for line in fr:
-            fw.write(line)
-            pbar.update(1)
+            data = json.loads(line)
+            if data['business_id'] in photo_info:
+                fw.write(line)
+                pbar.update(1)
+
             if pbar.n == sub_num - 1:
                 break
     fr.close()
--- src/subset/sub_photos.py
+++ src/subset/sub_photos.py
@@ -2,12 +2,18 @@ import json
 import config
 
 from tqdm import tqdm
+from subset import sub_businesses
 
 SUB_FILE = "./out/photos_subset_{}.json".format(config.SUBSET_SETTINGS['PERC'])
 
 def generate_subset(f_dir, perc):
     fr = open(f_dir, 'r')
     fw = open(SUB_FILE, 'w')
+
+    n_business_f = open(sub_businesses.SUB_FILE, 'r')
+    businesses = set([json.loads(d)['business_id'] for d in n_business_f])
+    n_business_f.close()
+
     num_lines = sum(1 for _ in fr)
     sub_num = int(perc * num_lines)
 
@@ -15,7 +21,7 @@ def generate_subset(f_dir, perc):
     with tqdm(total=sub_num) as pbar:
         for line in fr:
             data = json.loads(line)
-            if data["caption"] != "":
+            if data["caption"] != "" and data["business_id"] in businesses:
                 fw.write(line)
                 pbar.update(1)
 
